b86d0a5cc36e68b21e6ff2e4a9a59cd94cd0afa7
m0h4x <86525723+m0h4x@users.noreply.github.com>
2021-11-28 18:13:02 +0100

    ex1
    ex1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
+  const btnStart = document.querySelector('[data-start]')
+  const btnStop = document.querySelector('[data-stop]')
+  
+  let timerId = null;
+   
+  
+  btnStart.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        document.body.style.background = getRandomHexColor()
+        
+        if (timerId !== null) {
+            btnStart.setAttribute('disabled', true)       
+        }
+         }, 1000,);
+  });
+  
+  
+  btnStop.addEventListener('click', function onBtnClick() {
+      clearInterval(timerId)
+       if(btnStart.hasAttribute('disabled', true)) {
+            btnStart.removeAttribute('disabled', true)       
+        }
+  })
