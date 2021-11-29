2e3ad627a921d22d1dc54c75a35db27110e3ea0c
m0h4x <86525723+m0h4x@users.noreply.github.com>
2021-11-29 18:54:21 +0100

    promises
    promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,26 @@
+import Notiflix from 'notiflix'
+
+const form = document.querySelector('.form')
+const inputDelay = document.querySelector('[name = delay]')
+const step = document.querySelector('[name = step]')
+const amount = document.querySelector('[name = amount]')
+
+
+ form.addEventListener('submit', onFormSubmit)
+
 function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+  
   const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+ 
   if (shouldResolve) {
-    // Fulfill
+resolve({position, delay})
   } else {
-    // Reject
+  reject({position, delay})
   }
+                
+        }, delay)
+  })
 }
